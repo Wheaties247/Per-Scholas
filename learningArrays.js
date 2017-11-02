1b68f4455ec068b0580d8405b34e71db17cc751a
Wheaties247 <Timothylowe247@gmail.com>
2017-11-02 17:19:30 -0400

    Update learningArrays.js

--- learningArrays.js
+++ learningArrays.js
@@ -2,12 +2,12 @@ var rap=['Kendrik','Andre 3000','Black Star'];
 var rock=['Metallica','Stone Temple Pilots','Led Zepplin'];
 var alt=['Red Hot Chili Peppers','Incubus','Simon & Garfunkle'];
 
-// rap.push('J-Cole');
-// console.log(rap[0]);
-// console.log(rock[2]);
-// console.log(alt[2]);
-// console.log(rap);
-// console.log(rap.length);
+rap.push('J-Cole');
+console.log(rap[0]);
+console.log(rock[2]);
+console.log(alt[2]);
+console.log(rap);
+console.log(rap.length);
 for (alt.length; alt.length>0;alt.pop()){
 	//alt.pop();
 	console.log(alt);
